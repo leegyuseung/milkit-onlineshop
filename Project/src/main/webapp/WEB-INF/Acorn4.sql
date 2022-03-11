2a43326be231340c5e05cd790584c15a2ad2eda2
SUL <ini2854@naver.com>
2022-03-11 16:02:31 +0900

    sql문 작성

--- Project/src/main/webapp/WEB-INF/Acorn4.sql
+++ Project/src/main/webapp/WEB-INF/Acorn4.sql
@@ -20,3 +20,15 @@ CREATE TABLE product(
 	updateDate DATE, /* 상품 정보 업데이트일 */
 	buyCount NUMBER(10) /*인기 품목 선정을 위해*/
 );
+
+-- 게시글을 저장할 테이블 
+CREATE TABLE board_inquiry(
+	num NUMBER PRIMARY KEY, --글번호
+	writer VARCHAR2(100) NOT NULL, --작성자 (로그인된 아이디)
+	title VARCHAR2(100) NOT NULL, --제목
+	content CLOB, --글 내용
+	viewCount NUMBER, -- 조회수
+	regdate DATE --글 작성일
+);
+-- 게시글의 번호를 얻어낼 시퀀스
+CREATE SEQUENCE board_inquiry_seq; 
