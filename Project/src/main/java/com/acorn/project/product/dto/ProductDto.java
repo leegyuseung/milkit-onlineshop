ccdc5b411a535a5ad88c4a376036c29d11708e97
Heechan Kim <gmlcks7575@gmail.com>
2022-03-14 16:01:00 +0900

    private String imagepath 추가

--- Project/src/main/java/com/acorn/project/product/dto/ProductDto.java
+++ Project/src/main/java/com/acorn/project/product/dto/ProductDto.java
@@ -1,112 +1,103 @@
 package com.acorn.project.product.dto;
 
 public class ProductDto {
+	private String productId;
+	private String productName;
+	private int price;
+	private int stock;
+	private String productCate; //상품 카테고리
+	private String regDate;
+	private String updateDate;
+	private int buyCount;
+	private String ImagePath;
+	   
+	public ProductDto() {}
 
-   private String productId;
-   private String productName;
-   private int price;
-   private int stock;
-   private String productCate; //상품 카테고리
-   private String regDate;
-   private String updateDate;
-   private int buyCount;
+	public ProductDto(String productId, String productName, int price, int stock, String productCate, String regDate,
+			String updateDate, int buyCount, String imagePath) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.price = price;
+		this.stock = stock;
+		this.productCate = productCate;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.buyCount = buyCount;
+		ImagePath = imagePath;
+	}
 
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getProductCate() {
+		return productCate;
+	}
+
+	public void setProductCate(String productCate) {
+		this.productCate = productCate;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public int getBuyCount() {
+		return buyCount;
+	}
+
+	public void setBuyCount(int buyCount) {
+		this.buyCount = buyCount;
+	}
+
+	public String getImagePath() {
+		return ImagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		ImagePath = imagePath;
+	}
    
-   public ProductDto() {}
-
-
-public ProductDto(String productId, String productName, int price, int stock, String productCate, String regDate,
-		String updateDate, int buyCount) {
-	super();
-	this.productId = productId;
-	this.productName = productName;
-	this.price = price;
-	this.stock = stock;
-	this.productCate = productCate;
-	this.regDate = regDate;
-	this.updateDate = updateDate;
-	this.buyCount = buyCount;
-}
-
-
-public String getProductId() {
-	return productId;
-}
-
-
-public void setProductId(String productId) {
-	this.productId = productId;
-}
-
-
-public String getProductName() {
-	return productName;
-}
-
-
-public void setProductName(String productName) {
-	this.productName = productName;
-}
-
-
-public int getPrice() {
-	return price;
-}
-
-
-public void setPrice(int price) {
-	this.price = price;
-}
-
-
-public int getStock() {
-	return stock;
-}
-
-
-public void setStock(int stock) {
-	this.stock = stock;
-}
-
-
-public String getProductCate() {
-	return productCate;
-}
-
-
-public void setProductCate(String productCate) {
-	this.productCate = productCate;
-}
-
-
-public String getRegDate() {
-	return regDate;
-}
-
-
-public void setRegDate(String regDate) {
-	this.regDate = regDate;
-}
-
-
-public String getUpdateDate() {
-	return updateDate;
-}
-
-
-public void setUpdateDate(String updateDate) {
-	this.updateDate = updateDate;
-}
-
-
-public int getbuyCount() {
-	return buyCount;
-}
-
-
-public void setbuyCount(int buyCount) {
-	this.buyCount = buyCount;
-}
-
    
 }
