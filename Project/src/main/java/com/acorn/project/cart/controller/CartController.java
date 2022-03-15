903add5f2b4009b3dac4bb868b9cd1feb7f812e3
SUL <ini2854@naver.com>
2022-03-15 16:59:46 +0900

    cartController ㅠㅠ

--- Project/src/main/java/com/acorn/project/cart/controller/CartController.java
+++ Project/src/main/java/com/acorn/project/cart/controller/CartController.java
@@ -1,14 +1,19 @@
 package com.acorn.project.cart.controller;
 
-import javax.inject.Inject;
+
+
+import java.util.List;
+
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.acorn.project.cart.dao.CartDao;
+
 import com.acorn.project.cart.dto.CartDto;
 import com.acorn.project.cart.service.CartService;
 
@@ -19,18 +24,25 @@ public class CartController {
 	
 	
 	@RequestMapping("/private/cart/carthome")
-	public String cart(HttpSession session) {
-		String id=(String)session.getAttribute("id");
-		service.cartList(id);
+	public String cart() {
 		
 		return "cart/carthome";
 	}
+	
+	@RequestMapping("/private/cart/cartlist")
+	public String cartList(HttpSession session, ModelAndView mView) {
+		String id=(String)session.getAttribute("id");
+		List<CartDto> list=service.cartList(id);
+		mView.addObject("list", list);
+		return "cart/carthome";
+	}
+	
 	
 	@RequestMapping("/private/cart/insert.do")
 	public String insert(@ModelAttribute CartDto dto, HttpSession session) {
 		String id=(String)session.getAttribute("id");
 		dto.setId(id);
 		service.insert(dto);
-		return "redirect:/private/cart/carthome.do";
+		return "redirect:/private/cart/cartlist.do";
 	}
 }
